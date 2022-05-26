afaa4983791c2c060470725a02f0be79cb28e327
Christopher Zhong <christopher.zhong@icatalyst.com>
2022-05-26 15:29:12 +0800

    Initial commit

---
+++ sample.spec.js
@@ -0,0 +1,20 @@
+function emitWarningWithError() {
+    const error = new Error('foo')
+    error.name = 'name'
+    error.message = 'foo'
+    process.emitWarning(error)
+}
+
+function emitWarningWithString() {
+    process.emitWarning('foo')
+}
+
+describe('emitWarning', () => {
+    it('should not cause an error using Error', () => {
+        emitWarningWithError();
+    });
+
+    it('should not cause an error using a string', () => {
+        emitWarningWithString();
+    });
+});
